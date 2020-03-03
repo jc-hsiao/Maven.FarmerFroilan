a61cc0daf69c4709bf6e3238a4cd0c7f155bc9fa
Zanetta Norris <znorris@zipcodes-MBP-2.localdomain>
2020-03-03 16:29:13 -0500

    added getters and setters to crop class

--- src/main/java/com/zipcodewilmington/froilansfarm/Produce/CornStalk.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Produce/CornStalk.java
@@ -1,4 +1,9 @@
 package com.zipcodewilmington.froilansfarm.Produce;
 
+import com.zipcodewilmington.froilansfarm.Storage.CropRow;
+
+import java.util.ArrayList;
+
 public class CornStalk extends Crop {
+    ArrayList<CropRow> NewCropRow;
 }
