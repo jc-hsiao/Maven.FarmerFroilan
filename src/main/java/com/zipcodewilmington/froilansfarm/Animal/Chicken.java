cb62915af8fed5fd28058f27d0c6da1fdb3341d5
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-03 16:54:37 -0500

    updated chicken class

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
@@ -3,4 +3,8 @@ package com.zipcodewilmington.froilansfarm.Animal;
 import com.zipcodewilmington.froilansfarm.Interfaces.Produce;
 
 public class Chicken extends Animal implements Produce {
+
+    public Egg layEgg(){
+        //return egg object
+    }
 }
