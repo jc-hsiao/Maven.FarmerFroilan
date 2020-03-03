7866679cd6b0fb58037112087f8ca418eb162b01
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro-3.local>
2020-03-03 17:04:17 -0500

    completed person class and test

--- src/main/java/com/zipcodewilmington/froilansfarm/Person/Farmer.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person/Farmer.java
@@ -1,4 +1,18 @@
 package com.zipcodewilmington.froilansfarm.Person;
+import com.zipcodewilmington.froilansfarm.Animal.Animal;
+import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
 
 public class Farmer extends Botanist {
+
+    public Farmer(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    public void tame(Animal animal) {
+
+    }
+
+    public void fillFertilizer (CropDuster cropDuster) {
+
+    }
 }
