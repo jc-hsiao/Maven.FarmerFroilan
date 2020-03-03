b564031d671cd348711e7d25b4b84b093ae04714
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-03 17:50:44 -0500

    saving

--- src/main/java/com/zipcodewilmington/froilansfarm/Storage/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Storage/FarmHouse.java
@@ -1,8 +1,23 @@
 package com.zipcodewilmington.froilansfarm.Storage;
 
+import com.zipcodewilmington.froilansfarm.Person.Person;
+
+import java.util.ArrayList;
+
 public class FarmHouse {
 
-    public FarmHouse() {
+    ArrayList<Person> listOfPeople;
 
+    public FarmHouse() {
+        this.listOfPeople = new ArrayList<>();
     }
+
+    public void addPerson(Person newPerson) {
+        listOfPeople.add(newPerson);
+    }
+
+    public ArrayList<Person> getListOfPeople() {
+        return listOfPeople;
+    }
+
 }
