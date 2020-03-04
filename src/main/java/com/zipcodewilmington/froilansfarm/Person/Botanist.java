64ceea5cdf23a66645e5664f8fed6b52f248515d
Sandy Setiawan <ssetiawan@zipcodersMBP21.localdomain>
2020-03-04 15:02:00 -0500

    cleaned up some codes

--- src/main/java/com/zipcodewilmington/froilansfarm/Person/Botanist.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person/Botanist.java
@@ -11,7 +11,7 @@ public class Botanist extends Person implements Eater {
     }
 
     public void plant(Crop crop, CropRow cropRow) {
-        //cropRow.addCrop(crop);
+        cropRow.addSeeds(crop);
     }
 
     @Override
