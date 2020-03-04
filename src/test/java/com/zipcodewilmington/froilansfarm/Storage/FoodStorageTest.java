fb28f559c6666cac3db51f712d80fd4316d36ebe
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-04 10:51:23 -0500

    completed tests

--- src/test/java/com/zipcodewilmington/froilansfarm/Storage/FoodStorageTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Storage/FoodStorageTest.java
@@ -2,23 +2,58 @@ package com.zipcodewilmington.froilansfarm.Storage;
 
 import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
 import com.zipcodewilmington.froilansfarm.Interfaces.Produce;
+import com.zipcodewilmington.froilansfarm.Produce.StrangeFruit;
+import com.zipcodewilmington.froilansfarm.Produce.StrangePlant;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 public class FoodStorageTest {
+
     FoodStorage currentFoodStorage = new FoodStorage();
-    LinkedHashMap<String, ArrayList<Edible>> foodStorage;
-    ArrayList<Edible> foodToAdd = new ArrayList<>();
-    //Produce corn = new Corn();
-    String foodName = "Corn";
+    //LinkedHashMap<String, ArrayList<Edible>> foodStorage;
+    ArrayList<Edible> listOfFood = new ArrayList<>();
+    ArrayList<Edible> listOfPurpleFruit = new ArrayList<>();
+    Produce strangePlant = new StrangePlant();
+    Produce anotherStrangePlant = new StrangePlant();
+    Edible currentStrangeFruit = new StrangeFruit();
+    Edible anotherStrangeFruit = new StrangeFruit();
+    String foodName = "StrangePlant";
+    String purpleFruit = "purpleFruit";
 
     @Test
     public void foodStorageConstructorTest() {
-        //foodToAdd.add(corn);
+        listOfFood.add(currentStrangeFruit);
+        currentFoodStorage = new FoodStorage();
+        currentFoodStorage.addFoodSet(foodName, listOfFood);
+        Integer currentFoodStorageSize = currentFoodStorage.foodStorage.keySet().size();
+        Integer expectedFoodStorageSize = 1;
 
+        Assert.assertEquals(expectedFoodStorageSize, currentFoodStorageSize);
+    }
+
+    @Test
+    public void addNewFoodTest() {
+        listOfFood.add(anotherStrangeFruit);
+        currentFoodStorage.addFoodSet(foodName, listOfFood);
+        Integer currentFoodStorageSize = currentFoodStorage.foodStorage.keySet().size();
+        Integer expectedFoodStorageSize = 1;
+
+        Assert.assertEquals(expectedFoodStorageSize, currentFoodStorageSize);
+    }
+
+    @Test
+    public void updateFoodCount() {
+        listOfFood.add(currentStrangeFruit);
+        currentFoodStorage.addFoodSet(foodName, listOfFood);
+        currentFoodStorage.updateFoodCount(foodName, anotherStrangePlant, 10);
+        /*Integer a = currentFoodStorage.getFoodStorage().containsKey(foodName);
+        Integer g = 1;*/
+        Integer a = currentFoodStorage.getFoodStorage().get(foodName).size();
+        System.out.println(a);
+        Assert.assertTrue(currentFoodStorage.getFoodStorage().containsKey(foodName));
+        //Assert.assertEquals(a, g);
     }
 
 }
