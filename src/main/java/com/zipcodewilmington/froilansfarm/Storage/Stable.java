cc8f756181316347018331f2dbfe5a89d3528e06
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-03 21:35:09 -0500

    add stable methods and tests

--- src/main/java/com/zipcodewilmington/froilansfarm/Storage/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Storage/Stable.java
@@ -1,28 +1,62 @@
 package com.zipcodewilmington.froilansfarm.Storage;
 import com.zipcodewilmington.froilansfarm.Animal.Horse;
 import java.util.ArrayList;
-
 public class Stable {
 
     private ArrayList<Horse> horses;
+    boolean isClean = true;
 
     public Stable(){
         horses = new ArrayList<>();
     }
 
-    public void addHorse(Horse horse){
-        horses.add(horse);
+    public Stable(ArrayList<Horse> horses){
+        this.horses = horses;
     }
 
-    public void removeHorse(String name){
-        for( Horse h :horses ){
-
+    public void addHorse(Horse horse){
+        horses.add(horse);
+        //when you add horse there is a change that stable gets dirty
+        if(Math.random()<0.2){
+            getsDirty();
         }
     }
 
+    public Horse removeHorse(String name){
+        Horse h = getHorse(name);
+        horses.remove(h);
+        return h;
+    }
 
-    public int checkNumOfHorse(){
+    public Horse getHorse(String name){
+        for( Horse h :horses ){
+            if(h.getName().equals(name)){
+                return h;
+            }
+        }
+        return null;
+    }
+
+    public int getNumOfHorse(){
         return horses.size();
+    }
+
+    //you can only clean it when there are no horse
+    public void cleanUpStable(){
+        if(getNumOfHorse()==0)
+            isClean = true;
+    }
+
+    public void getsDirty(){
+        isClean = false;
+    }
+
+    public String getListOfHorseName(){
+        StringBuilder s = new StringBuilder();
+        for(Horse h :horses){
+            s.append(h.getName()).append("\n");
+        }
+        return s.toString();
     }
 
 
--- src/test/java/com/zipcodewilmington/froilansfarm/Storage/StableTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Storage/StableTest.java
@@ -1,4 +1,34 @@
 package com.zipcodewilmington.froilansfarm.Storage;
 
+import com.zipcodewilmington.froilansfarm.Animal.Horse;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class StableTest {
+
+    @Test
+    public void constructor1Test(){
+        Stable s = new Stable();
+        Assert.assertEquals(0, s.getNumOfHorse());
+        Assert.assertNull(s.removeHorse("Bullet"));
+    }
+
+    @Test
+    public void constructor2Test(){
+        Stable s;
+        ArrayList<Horse> horseArrayL = new ArrayList<>();
+        Horse horse1 = new Horse("Bullet", "Black", 2);
+        Horse horse2 = new Horse("Epona", "Brown", 3);
+        horseArrayL.add(horse1);
+        horseArrayL.add(horse2);
+        s = new Stable(horseArrayL);
+
+        Assert.assertEquals(2, s.getNumOfHorse());
+        Assert.assertEquals( horse1,s.getHorse("Bullet"));
+        Assert.assertEquals( horse2,s.getHorse("Epona"));
+
+    }
+
 }
