c49d11be9f42886c2feee712827d995d35dce15d
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-04 17:34:52 -0500

    saving

--- src/main/java/com/zipcodewilmington/froilansfarm/Simulation/Events/MondayEvents.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Simulation/Events/MondayEvents.java
@@ -3,14 +3,18 @@ import com.zipcodewilmington.froilansfarm.Person.Botanist;
 import com.zipcodewilmington.froilansfarm.Person.Farmer;
 import com.zipcodewilmington.froilansfarm.Person.Person;
 import com.zipcodewilmington.froilansfarm.Storage.Farm;
+import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
+import com.zipcodewilmington.froilansfarm.Vehicle.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.Vehicle.Vehicle;
 
 public class MondayEvents implements Events{
-    public void run(Farm farm){
+    public void run(Farm farm) {
         Person froilanda = (Botanist)farm.getFarmHouse().getPerson("Froilanda");
         //things happens ONLY on Monday will start here
         c.printlnWait(froilanda.getName() + " gets into the Crop Duster.");
-
-
+        FarmVehicle cropDuster = farm.getVehicles().get(1);
+        CropDuster temp = (CropDuster)cropDuster;
+        temp.fertilizeCrops(farm.getFields().get(0), 1);
     }
 
 }
