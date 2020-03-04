f096be7207f87f0987a8a060bc9135d24c85144c
Raheel Uppal <ruppal@zipcodes-MacBook-Pro.local>
2020-03-04 17:43:56 -0500

    Tuesday

--- src/main/java/com/zipcodewilmington/froilansfarm/Simulation/Events/TuesdayEvents.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Simulation/Events/TuesdayEvents.java
@@ -1,8 +1,11 @@
 package com.zipcodewilmington.froilansfarm.Simulation.Events;
+import com.zipcodewilmington.froilansfarm.Produce.Crop;
 import com.zipcodewilmington.froilansfarm.Storage.CropRow;
 import com.zipcodewilmington.froilansfarm.Storage.Farm;
 import com.zipcodewilmington.froilansfarm.Storage.Field;
 import com.zipcodewilmington.froilansfarm.Vehicle.Tractor;
+
+import java.util.ArrayList;
 
 public class TuesdayEvents implements Events {
 
@@ -13,8 +16,14 @@ public class TuesdayEvents implements Events {
         Field field = farm.getFields().get(0);
         CropRow newCropRow = field.getCropRow(0);
         Tractor tractor = (Tractor) farm.getVehicles().get(0);
-        tractor.harvestCrops(newCropRow);
 
+        ArrayList<Crop> harvestedCrops = new ArrayList<>();
+
+        harvestedCrops = tractor.harvestCrops(newCropRow);
+
+        for(Crop c : harvestedCrops){
+            System.out.println(c.getClass().getSimpleName());
+        }
 
     }
 }
