a2f0d1aebdb4022b4cf6a96449836f5b32e7a741
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-04 15:24:36 -0500

    added a take food function

--- src/main/java/com/zipcodewilmington/froilansfarm/Storage/FoodStorage.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Storage/FoodStorage.java
@@ -24,6 +24,12 @@ public class FoodStorage {
         return foodStorage;
     }
 
+    public void takeFood(String foodName, Edible food) {
+        if (foodStorage.containsKey(foodName)) {
+            foodStorage.get(foodName).remove(food);
+        }
+    }
+
     public void updateFoodCount(String foodName, Produce newProduce, int howMany) {
         if (foodStorage.containsKey(foodName)) {
             for (int i = 0; i < howMany; i++) {
