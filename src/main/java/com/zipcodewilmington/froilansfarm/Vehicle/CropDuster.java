9b10cb9335094dcaae1ab59fb37bdeaca43d6550
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-04 22:04:30 -0500

    completed MondayEvents

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDuster.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 public class CropDuster extends FarmVehicle implements Flyable {
     private static final Logger LOGGER = Logger.getLogger(CropDuster.class.getName());
 
-    Integer fertilizerAmount = 8;
+    Integer fertilizerAmount = 5000;
 
     public CropDuster(Integer id, String color) {
         super(id, color);
@@ -19,8 +19,8 @@ public class CropDuster extends FarmVehicle implements Flyable {
 
 
     public Boolean hasFertilizer() {
-
-        if (fertilizerAmount == 0) {
+        if (fertilizerAmount < 1) {
+            LOGGER.info("\nYou are out of fertilizer. Please refill.");
             return false;
         }
         return true;
@@ -46,7 +46,7 @@ public class CropDuster extends FarmVehicle implements Flyable {
 
     public Integer getFertilizerAmount() {
 
-        if (fertilizerAmount < 0) {
+        if (fertilizerAmount < 1) {
             System.out.println("Out of fertilizer. You are fertilizing nothing now.");
         } else {
             return fertilizerAmount;
@@ -60,18 +60,15 @@ public class CropDuster extends FarmVehicle implements Flyable {
 
     public void refillFertilizer() {
         setFertilizerAmount(8);
-
     }
 
     @Override
     public void fly() {
-
         setGasLevel(getGasLevel() - 1);
     }
 
     @Override
     public String makeNoise() {
-
         return "puh puh puh";
     }
 }
